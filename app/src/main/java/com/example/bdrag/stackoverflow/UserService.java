d4fbfcb66dc8db9648e3b4145b02fd353f3727f1
bdragos678 <bdragos678@gmail.com>
2018-03-01 22:13:38 +0200

    Am adaugat clasa care face apelul catre API

--- app/src/main/java/com/example/bdrag/stackoverflow/UserService.java
+++ app/src/main/java/com/example/bdrag/stackoverflow/UserService.java
@@ -1,7 +1,11 @@
 package com.example.bdrag.stackoverflow;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface UserService {
 
-
-
+    @GET("2.2/users")
+    public Call<User> getUsers(@Query("pagesize") int pagesize, @Query("order") String order, @Query("sort") String sort, @Query("site") String site);
 }
